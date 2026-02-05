8ba3d4c5df3cf5ca253453ed0310e54d7a2aa469
Morgan McKay <168682084+mossygrave@users.noreply.github.com>
2026-02-05 08:48:44 -0700

    Delete scripts directory

--- scripts/player.gd
+++
@@ -1,9 +0,0 @@
-extends Camera3D
-
-var speed: float = 250.0
-
-func _unhandled_input(event):
-	if event is InputEventMouseMotion:
-		rotation_degrees.y -= event.relative.x * 0.25
-		$Camera3D.rotation_degrees.x -= event.relative.y * 0.25
-		
