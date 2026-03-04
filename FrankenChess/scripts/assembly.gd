3e86520ba3d2c1daadd7deba10c5453808f6cac6
Morgan McKay <168682084+mossygrave@users.noreply.github.com>
2026-03-03 20:18:34 -0700

    created mid part scenes

--- FrankenChess/scripts/assembly.gd
+++ FrankenChess/scripts/assembly.gd
@@ -17,6 +17,7 @@ extends Node3D
 	{ "path": "res://assets/Chess Pieces/Knight Piece/Knight-Bottom.glb", "type": "base"},
 	{ "path": "res://scenes/middle parts/pawn_mid.tscn", "type": "middle"},
 	{ "path": "res://scenes/middle parts/bishop_mid.tscn", "type": "middle"},
+	{ "path": "res://scenes/middle parts/knight_mid.tscn", "type": "middle"},
 	{ "path": "res://assets/Chess Pieces/Pawn Piece/Pawn-Top.glb", "type": "top"},
 	{ "path": "res://assets/Chess Pieces/Bishop Piece/Bishop-Top.glb", "type": "top"},
 	{ "path": "res://assets/Chess Pieces/King Piece/King-Top.glb", "type": "top"}
