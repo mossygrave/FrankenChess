7157c5bf5c20fe43f50c007989063956500eaf8c
MasterSmeddy <ethanyeldems@gmail.com>
2026-03-11 19:50:35 -0600

    Game Over Menu V.1 Complete
    Game Over Menu instantiated with Restart, return to Main Menu, and Quit options. Currently, this only appears after clicking the "Change Scene" button in the main scene. When win/lose conditions arise, we'll have to come back to "game_over.tscn" and update it.

--- FrankenChess/scenes/game_over.gd
+++ FrankenChess/scenes/game_over.gd
@@ -1,5 +1,11 @@
 extends CanvasLayer
 
 
-func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/MainMenuTesting.tscn");pass # Replace with function body.
+func _on_restart_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
+
+func _on_main_menu_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/MainMenuTesting.tscn")
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
